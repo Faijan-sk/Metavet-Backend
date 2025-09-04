a76f285a58d71770f50495e6e82ce1a69e7a8b29
Faijan-sk <shaikhfaizan.jb@gmail.com>
2025-09-04 21:13:23 +0530

    login controller

--- src/main/java/com/example/demo/Controller/LoginAuthController.java
+++ src/main/java/com/example/demo/Controller/LoginAuthController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,8 @@ import com.example.demo.Service.LoginAuthService;
 
 
 @RestController
-@RequestMapping("/auth")
+@Controller
+@RequestMapping("/api/auth")
 public class LoginAuthController {
 
     @Autowired
